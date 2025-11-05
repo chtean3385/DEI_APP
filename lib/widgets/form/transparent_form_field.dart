7cae09b88988b71c0abff42a0b6878f2e8b1028b
Greejith VC <greejithvc@gmail.com>
2025-11-05 12:30:12 +0530

    counter text

--- lib/widgets/form/transparent_form_field.dart
+++ lib/widgets/form/transparent_form_field.dart
@@ -15,6 +15,7 @@ class TransparentFormField extends StatefulWidget {
   final TextCapitalization textCapitalization;
   final Iterable<String>? autofillHints;
   final TextInputAction? textInputAction;
+
   // final ValueChanged<String>? onFieldSubmitted;
   final List<TextInputFormatter>? inputFormatters;
   final FocusNode? focusNode;
@@ -23,8 +24,12 @@ class TransparentFormField extends StatefulWidget {
   final int? minLines;
   final int? maxLength;
   final double? radius;
-  final bool readOnly ;
-  final bool isRequired ;
+  final bool readOnly;
+
+  final bool isRequired;
+
+  final bool showCounter;
+
   final Color? fillColor;
   final ValueChanged<String>? onFieldSubmitted;
 
@@ -32,7 +37,7 @@ class TransparentFormField extends StatefulWidget {
     super.key,
     required this.controller,
     required this.hint,
-     this.icon,
+    this.icon,
 
     this.isPassword = false,
     this.onTogglePassword,
@@ -51,6 +56,7 @@ class TransparentFormField extends StatefulWidget {
     this.radius,
     this.readOnly = false,
     this.isRequired = false,
+    this.showCounter = false,
     this.fillColor,
     this.onFieldSubmitted,
   });
@@ -61,6 +67,20 @@ class TransparentFormField extends StatefulWidget {
 
 class _TransparentFormFieldState extends State<TransparentFormField> {
   bool _isPasswordVisible = false;
+
+  void _onTextChanged() => setState(() {});
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.showCounter == true) widget.controller.addListener(_onTextChanged);
+  }
+
+  @override
+  void dispose() {
+    if (widget.showCounter == true) widget.controller.removeListener(_onTextChanged);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,21 +99,23 @@ class _TransparentFormFieldState extends State<TransparentFormField> {
               ),
               children: widget.isRequired
                   ? [
-                TextSpan(
-                  text: ' *',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ]
+                      TextSpan(
+                        text: ' *',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]
                   : [],
             ),
           ),
         if (widget.label != null) gapH4(),
         DecoratedBox(
           decoration: BoxDecoration(
-            color:widget.fillColor ?? AppColors.primaryColor.withValues(alpha: 0.15),
+            color:
+                widget.fillColor ??
+                AppColors.primaryColor.withValues(alpha: 0.15),
             borderRadius: BorderRadius.circular(widget.radius ?? 16),
             border: Border.all(
               color: AppColors.primaryColor.withValues(alpha: 0.13),
@@ -122,31 +144,45 @@ class _TransparentFormFieldState extends State<TransparentFormField> {
                 color: Colors.black,
                 fontWeight: FontWeight.normal,
               ),
-              onFieldSubmitted: widget.onFieldSubmitted ??  (_) {
-                if (widget.nextFocusNode != null) {
-                  // Safe check: only move if nextNode is attached
-                  if (widget.nextFocusNode!.hasFocus == false && widget.nextFocusNode!.context != null) {
-                    FocusScope.of(context).requestFocus(widget.nextFocusNode);
-                  } else {
-                    FocusScope.of(context).unfocus();
-                  }
-                } else {
-                  FocusScope.of(context).unfocus();
-                }
-              },
+              onFieldSubmitted:
+                  widget.onFieldSubmitted ??
+                  (_) {
+                    if (widget.nextFocusNode != null) {
+                      // Safe check: only move if nextNode is attached
+                      if (widget.nextFocusNode!.hasFocus == false &&
+                          widget.nextFocusNode!.context != null) {
+                        FocusScope.of(
+                          context,
+                        ).requestFocus(widget.nextFocusNode);
+                      } else {
+                        FocusScope.of(context).unfocus();
+                      }
+                    } else {
+                      FocusScope.of(context).unfocus();
+                    }
+                  },
 
               decoration: InputDecoration(
                 hintText: widget.hint,
                 hintStyle: theme.textTheme.bodyMedium?.copyWith(
                   color: Colors.black54,
                 ),
-                counterText: "",
+                counterText: (widget.showCounter)
+                    ? "${widget.controller.text.length} / ${widget.maxLength}"
+                    : null,
+                // 👈 live counter
+                counterStyle: theme.textTheme.bodySmall?.copyWith(
+                  color: Colors.grey[600],
+                  fontSize: 12,
+                ),
                 errorStyle: theme.textTheme.displaySmall?.copyWith(
                   color: Colors.redAccent,
                   fontWeight: FontWeight.normal,
                   fontSize: 10,
                 ),
-                prefixIcon:widget.icon != null ? Icon(widget.icon, color: Colors.black54, size: 22) : null,
+                prefixIcon: widget.icon != null
+                    ? Icon(widget.icon, color: Colors.black54, size: 22)
+                    : null,
                 suffixIcon: widget.isPassword
                     ? IconButton(
                         icon: Icon(
