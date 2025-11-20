9acbda8cb161828c316f345d2208840b38c15616
Greejith VC <greejithvc@gmail.com>
2025-11-20 10:24:07 +0530

    revert theme changes - to keep app them  in seperat branch

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,8 @@ class _MyAppState extends ConsumerState<MyApp> {
     return MaterialApp(
       navigatorKey: navigatorKey,
       title: AppStrings.appName,
-      theme: lightTheme(context, accessibility),
-      darkTheme: darkTheme(context, accessibility),
+      theme: lightTheme(context),
+      darkTheme: darkTheme(context),
       themeMode: themeMode,
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
--- lib/ui/pages/employee_settings/settings_view.dart
+++ lib/ui/pages/employee_settings/settings_view.dart
@@ -29,92 +29,92 @@ class SettingsBody extends ConsumerWidget {
         // -----------------------------------
         // APPEARANCE TITLE
         // -----------------------------------
-        const SectionTitle("Appearance"),
-
-        // -----------------------------------
-        // THEME SWITCH
-        // -----------------------------------
-        SettingToggle(
-          title: themeMode == ThemeMode.dark ? "Dark Mode" : "Light Mode",
-          subtitle: "Switch between dark and light themes",
-          value: themeMode == ThemeMode.dark,
-          icon: themeMode == ThemeMode.dark ? Icons.dark_mode : Icons.light_mode,
-          onChanged: (val) {
-            themeController.state = val ? ThemeMode.dark : ThemeMode.light;
-          },
-        ),
+        // const SectionTitle("Appearance"),
+        //
+        // // -----------------------------------
+        // // THEME SWITCH
+        // // -----------------------------------
+        // SettingToggle(
+        //   title: themeMode == ThemeMode.dark ? "Dark Mode" : "Light Mode",
+        //   subtitle: "Switch between dark and light themes",
+        //   value: themeMode == ThemeMode.dark,
+        //   icon: themeMode == ThemeMode.dark ? Icons.dark_mode : Icons.light_mode,
+        //   onChanged: (val) {
+        //     // themeController.state = val ? ThemeMode.dark : ThemeMode.light;
+        //   },
+        // ),
 
         // -----------------------------------
         // FONT SIZE
         // -----------------------------------
 
-        Card(
-          elevation: 2,
-          color: Colors.white,
-          shadowColor: AppColors.primaryColor,
-          surfaceTintColor: AppColors.primaryColor,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          child: ListTile(
-            leading: Icon(Icons.format_size, color: AppColors.primaryColor),
-            title: Text(
-              "Font Size",
-              style: context.textTheme.labelMedium?.copyWith(height: 2),
-            ),
-            subtitle: Text(
-              "Adjust text size across the app",
-              style: context.textTheme.bodyMedium?.copyWith(color: Colors.black54),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.remove,color: Colors.black,),
-                  onPressed: accController.decreaseFont,
-                ),
-                Text( (accessibility.fontScale * 16).round().toString(),style: context.textTheme.bodyMedium,),
-                IconButton(
-                  icon: Icon(Icons.add,color: Colors.black,),
-                  onPressed: accController.increaseFont,
+        // Card(
+        //   elevation: 2,
+        //   color: Colors.white,
+        //   shadowColor: AppColors.primaryColor,
+        //   surfaceTintColor: AppColors.primaryColor,
+        //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        //   child: ListTile(
+        //     leading: Icon(Icons.format_size, color: AppColors.primaryColor),
+        //     title: Text(
+        //       "Font Size",
+        //       style: context.textTheme.labelMedium?.copyWith(height: 2),
+        //     ),
+        //     subtitle: Text(
+        //       "Adjust text size across the app",
+        //       style: context.textTheme.bodyMedium?.copyWith(color: Colors.black54),
+        //     ),
+        //     trailing: Row(
+        //       mainAxisSize: MainAxisSize.min,
+        //       children: [
+        //         IconButton(
+        //           icon: Icon(Icons.remove,color: Colors.black,),
+        //           onPressed: accController.decreaseFont,
+        //         ),
+        //         Text( (accessibility.fontScale * 16).round().toString(),style: context.textTheme.bodyMedium,),
+        //         IconButton(
+        //           icon: Icon(Icons.add,color: Colors.black,),
+        //           onPressed: accController.increaseFont,
+        //
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
 
-                ),
-              ],
-            ),
-          ),
-        ),
-
-        Card(
-          elevation: 2,
-          color: Colors.white,
-          shadowColor: AppColors.primaryColor,
-          surfaceTintColor: AppColors.primaryColor,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          child: ListTile(
-            leading: Icon(Icons.color_lens, color: AppColors.primaryColor),
-            title: Text(
-              "Button Color",
-              style: context.textTheme.labelMedium?.copyWith(height: 2),
-            ),
-            subtitle: Text(
-              "Choose your preferred button color",
-              style: context.textTheme.bodyMedium?.copyWith(color: Colors.black54),
-            ),
-            trailing: GestureDetector(
-              onTap: () async {
-                final selectedColor = await showDialog<Color>(
-                  context: context,
-                  builder: (_) => ButtonColorDialog(),
-                );
-                if (selectedColor != null) {
-                  settingsCtrl.setButtonColor(selectedColor);
-                }
-              },
-              child: CircleAvatar(
-                radius: 14,
-                backgroundColor: settings.buttonColor,
-              ),
-            ),
-          ),
-        ),
+        // Card(
+        //   elevation: 2,
+        //   color: Colors.white,
+        //   shadowColor: AppColors.primaryColor,
+        //   surfaceTintColor: AppColors.primaryColor,
+        //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        //   child: ListTile(
+        //     leading: Icon(Icons.color_lens, color: AppColors.primaryColor),
+        //     title: Text(
+        //       "Button Color",
+        //       style: context.textTheme.labelMedium?.copyWith(height: 2),
+        //     ),
+        //     subtitle: Text(
+        //       "Choose your preferred button color",
+        //       style: context.textTheme.bodyMedium?.copyWith(color: Colors.black54),
+        //     ),
+        //     trailing: GestureDetector(
+        //       onTap: () async {
+        //         final selectedColor = await showDialog<Color>(
+        //           context: context,
+        //           builder: (_) => ButtonColorDialog(),
+        //         );
+        //         if (selectedColor != null) {
+        //           settingsCtrl.setButtonColor(selectedColor);
+        //         }
+        //       },
+        //       child: CircleAvatar(
+        //         radius: 14,
+        //         backgroundColor: settings.buttonColor,
+        //       ),
+        //     ),
+        //   ),
+        // ),
 
         // -----------------------------------
         // ALERTS SECTION
